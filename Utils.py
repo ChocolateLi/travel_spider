0a1f34d839139a8b047e4f619e4321387f52bf37
chenli <954357018@qq.com>
2022-10-07 14:25:19 +0800

    修改一些爬虫程序

--- Utils.py
+++ Utils.py
@@ -24,16 +24,35 @@ class Utils():
     # 定义一个数据预处理的方法
     # 因为游记文本内容有太多不需要的符号了，以及太多空格
     def clear(self,text):
-        text.replace(u'\xa0', u'').replace('\n', '').replace('\r', '').replace(' ','').strip()
+        text.replace('\n', '').replace('\r', '').replace(' ','').strip()
         return self.re_obj.sub("", text)
 
 # if __name__ == '__main__':
-    # utils = Utils();
-    # str = """"""
-    # print(len(str))
-    # print("---------------------------")
-    # str = utils.clear(str)
-    # print(len(str))
+#     utils = Utils();
+#     str = """                                                                桂林小巷子青旅
+                                                            
+#                                                                 七星景区
+                                                            
+#                                                                 七星花桥
+                                                            
+#                                                                 漓江边
+                                                            
+#                                                                 漓江
+                                                            
+#                                                                 象鼻山
+                                                            
+#                                                                 象鼻山景
+#                                                                 漓江边道路
+
+#                                                                 路边演唱
+
+#                                                             再次来桂林却碰上国庆长假，小逛了下，到处的人，加上天热，逛得没劲啊
+                                                            
+#                                                                 桂林火车站"""
+#     print(len(str))
+#     print("---------------------------")
+#     str = utils.clear(str)
+#     print(len(str))
 
     # str = "   哈哈哈   怎么了，   干嘛  "
     # print(str)
